bf1403d9a8aa568750642c7ea306fc338449a4ca
avashshrestha89 <avashshresthausa@gmail.com>
2026-08-18 14:29:38 +0545

    Update generate-script.js

--- pages/api/generate-script.js
+++ pages/api/generate-script.js
@@ -26,7 +26,7 @@ Rules:
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.SWORAIAPIKEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.SWORAIAPIKEY}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
